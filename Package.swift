3acd4b678cbb470d74362d05d3f2ec8a889ed044
Gwynne Raskind <gwynne@darkrainfall.org>
2023-12-01 00:14:47 -0600

    Sendable cleanup (#1)

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version: 5.8
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "async-helpers",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     products: [
-        .library(
-            name: "AsyncHelpers",
-            targets: ["AsyncHelpers"]),
+        .library(name: "AsyncHelpers", targets: ["AsyncHelpers"]),
     ],
     targets: [
-        .target(name: "AsyncHelpers"),
+        .target(
+            name: "AsyncHelpers",
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency=complete")]
+        ),
     ]
 )
